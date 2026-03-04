4cd30e9910db12b238cd2de643182d7df4d844b8
ken-prime628 <kennedywaturi@gmail.com>
2026-03-04 10:18:41 +0300

    initial commit

--- careapp/urls.py
+++ careapp/urls.py
@@ -6,7 +6,7 @@ from careapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home ,name='home'),
+    path('home/', views.home ,name='home'),
     path('starter/', views.starter , name='starter'),
      path('appointment/', views.appointment , name='appointment'),
      path('about/', views.about , name='about'),
@@ -25,8 +25,8 @@ urlpatterns = [
     path('transactions/', views.transactions_list, name='transactions'),
 
     #Authentication
-    path('register/', views.register , name='register'),
-    path('login/', views.login , name='login'),
+    path('', views.register , name='register'),
+    path('login/', views.login_view , name='login'),
 
 
 
--- careapp/views.py
+++ careapp/views.py
@@ -5,6 +5,11 @@ from requests.auth import HTTPBasicAuth
 import json
 from careapp.credentials import MpesaAccessToken, LipanaMpesaPpassword
 
+#Authentication imports
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 from careapp.models import *
 
@@ -209,8 +214,49 @@ def transactions_list(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check the password
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+
+                # Display a message
+                messages.success(request, "Account created successfully")
+                return redirect('/login')
+            except:
+                # Display a message if the above fails
+                messages.error(request, "Username already exist")
+        else:
+            # Display a message saying passwords don't match
+            messages.error(request, "Passwords do not match")
+
     return render(request, 'register.html')
 
 
-def login(request):
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user exists
+        if user is not None:
+            # login(request, user)
+            login(request,user)
+            messages.success(request, "You are now logged in!")
+            # Admin
+            if user.is_superuser:
+                return redirect('/show')
+
+            # For Normal Users
+            return redirect('/home')
+        else:
+            messages.error(request, "Invalid login credentials")
+
     return render(request, 'login.html')
